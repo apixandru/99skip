7d46d4d8d8299537e0fe59293e4fa3a320a4e277
Alexandru <contact@apixandru.com>
2021-10-20 08:51:53 -0700

    fd

---
+++ assets/sources/org/apache/commons/commons-lang3/3.12.0/org/apache/commons/lang3/event/package-info.java
@@ -0,0 +1,59 @@
+<div class="file-content code js-syntax-highlight white" id="blob-content">
+    <div class="line-numbers">
+        <a class="file-line-num diff-line-num"
+           data-line-number="1"
+           id="L1" href="#L1">1</a><a class="file-line-num diff-line-num"
+           data-line-number="2"
+           id="L2" href="#L2">2</a><a class="file-line-num diff-line-num"
+           data-line-number="3"
+           id="L3" href="#L3">3</a><a class="file-line-num diff-line-num"
+           data-line-number="4"
+           id="L4" href="#L4">4</a><a class="file-line-num diff-line-num"
+           data-line-number="5"
+           id="L5" href="#L5">5</a><a class="file-line-num diff-line-num"
+           data-line-number="6"
+           id="L6" href="#L6">6</a><a class="file-line-num diff-line-num"
+           data-line-number="7"
+           id="L7" href="#L7">7</a><a class="file-line-num diff-line-num"
+           data-line-number="8"
+           id="L8" href="#L8">8</a><a class="file-line-num diff-line-num"
+           data-line-number="9"
+           id="L9" href="#L9">9</a><a class="file-line-num diff-line-num"
+           data-line-number="10"
+           id="L10" href="#L10">10</a><a class="file-line-num diff-line-num"
+           data-line-number="11"
+           id="L11" href="#L11">11</a><a class="file-line-num diff-line-num"
+           data-line-number="12"
+           id="L12" href="#L12">12</a><a class="file-line-num diff-line-num"
+           data-line-number="13"
+           id="L13" href="#L13">13</a><a class="file-line-num diff-line-num"
+           data-line-number="14"
+           id="L14" href="#L14">14</a><a class="file-line-num diff-line-num"
+           data-line-number="15"
+           id="L15" href="#L15">15</a><a class="file-line-num diff-line-num"
+           data-line-number="16"
+           id="L16" href="#L16">16</a><a class="file-line-num diff-line-num"
+           data-line-number="17"
+           id="L17" href="#L17">17</a><a class="file-line-num diff-line-num"
+           data-line-number="18"
+           id="L18" href="#L18">18</a><a class="file-line-num diff-line-num"
+           data-line-number="19"
+           id="L19" href="#L19">19</a><a class="file-line-num diff-line-num"
+           data-line-number="20"
+           id="L20" href="#L20">20</a><a class="file-line-num diff-line-num"
+           data-line-number="21"
+           id="L21" href="#L21">21</a><a class="file-line-num diff-line-num"
+           data-line-number="22"
+           id="L22" href="#L22">22</a>
+    </div>
+    <div class="blob-content"
+         data-blob-id="5afbd23d0e021a57af5234311b7287d5ca25c727"
+         data-path="src/main/java/main/repo/EmployeeRepo.java"
+         data-qa-selector="file_content">
+<pre class="code highlight">
+<code>
+<span class="line" lang="java" id="LC1"><span class="cm">/*</span></span><span class="line" lang="java" id="LC2"><span class="cm"> * Licensed to the Apache Software Foundation (ASF) under one or more</span></span><span class="line" lang="java" id="LC3"><span class="cm"> * contributor license agreements.  See the NOTICE file distributed with</span></span><span class="line" lang="java" id="LC4"><span class="cm"> * this work for additional information regarding copyright ownership.</span></span><span class="line" lang="java" id="LC5"><span class="cm"> * The ASF licenses this file to You under the Apache License, Version 2.0</span></span><span class="line" lang="java" id="LC6"><span class="cm"> * (the &quot;License&quot;); you may not use this file except in compliance with</span></span><span class="line" lang="java" id="LC7"><span class="cm"> * the License.  You may obtain a copy of the License at</span></span><span class="line" lang="java" id="LC8"><span class="cm"> *</span></span><span class="line" lang="java" id="LC9"><span class="cm"> *      http://www.apache.org/licenses/LICENSE-2.0</span></span><span class="line" lang="java" id="LC10"><span class="cm"> *</span></span><span class="line" lang="java" id="LC11"><span class="cm"> * Unless required by applicable law or agreed to in writing, software</span></span><span class="line" lang="java" id="LC12"><span class="cm"> * distributed under the License is distributed on an &quot;AS IS&quot; BASIS,</span></span><span class="line" lang="java" id="LC13"><span class="cm"> * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.</span></span><span class="line" lang="java" id="LC14"><span class="cm"> * See the License for the specific language governing permissions and</span></span><span class="line" lang="java" id="LC15"><span class="cm"> * limitations under the License.</span></span><span class="line" lang="java" id="LC16"><span class="cm"> */</span></span><span class="line" lang="java" id="LC17"><span class="cm">/**</span></span><span class="line" lang="java" id="LC18"><span class="cm"> * Provides some useful event-based utilities.</span></span><span class="line" lang="java" id="LC19"><span class="cm"> *</span></span><span class="line" lang="java" id="LC20"><span class="cm"> * @since 3.0</span></span><span class="line" lang="java" id="LC21"><span class="cm"> */</span></span><span class="line" lang="java" id="LC22"><span class="kn">package</span><span> </span><span>org.apache.commons.lang3.event</span><span>;</span></span>
+</code>
+</pre>
+    </div>
+</div>
